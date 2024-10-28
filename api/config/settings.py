76599df0133d7b04b357b3e38572a3c3c72b4954
vlodkow <vlodkow@gmail.com>
2024-10-28 11:06:47 +0200

    Update prod config

--- api/config/settings.py
+++ api/config/settings.py
@@ -10,8 +10,8 @@ class Config:
     MODELS_LIST = ['models.block_model', 'models.receipt_action_model', 'models.transaction_model', ]
     DEFAULT_PAGE_LIMIT = 100
     AI_MODEL_ID = "config/ai_models/Llama-3.2-3B-Instruct"
-    AI_SQL_MAX_TOKENS = 250
-    AI_SQL_NUM_BEAMS = 3
+    AI_SQL_MAX_TOKENS = 300
+    AI_SQL_NUM_BEAMS = 4
     AI_SUMMARY_MAX_TOKENS = 400
     AI_SUMMARY_NUM_BEAMS = 1
 
